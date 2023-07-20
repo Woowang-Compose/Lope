156e39d3ec295c48acf5d68749c897c57a667f5e
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2023-07-20 15:59:54 +0900

    feat: week3 주차 과제1 구현

--- Unit1/Pathway3/TaskCompleted/app/src/main/java/com/example/taskcompleted/MainActivity.kt
+++ Unit1/Pathway3/TaskCompleted/app/src/main/java/com/example/taskcompleted/MainActivity.kt
@@ -5,32 +5,65 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.taskcompleted.ui.theme.TaskCompletedTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { }
+        setContent {
+            TaskCompletedScreen()
+        }
     }
 }
 
 @Composable
 fun TaskCompletedScreen() {
-    Column( ) { }
+    val image = painterResource(id = R.drawable.ic_task_completed)
+    val firstText = stringResource(id = R.string.all_task_completed)
+    val secondText = stringResource(id = R.string.nice_work)
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        contentImage(image)
+        contentText(firstText, secondText)
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() { }
+fun contentImage(painter: Painter) {
+    Image(
+        painter = painter,
+        contentDescription = null,
+        alignment = Alignment.TopCenter,
+    )
+}
+
+@Composable
+fun contentText(first: String, second: String) {
+    Text(
+        text = first,
+        fontWeight = FontWeight.Bold,
+        modifier = Modifier.padding(top = 24.dp, bottom = 8.dp),
+    )
+    Text(
+        text = second,
+        fontSize = 16.sp,
+    )
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun DefaultPreview() {
+    TaskCompletedScreen()
+}
